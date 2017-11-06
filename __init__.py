034287d5bb136b596593ba6b4496f4ed443df1ed
Rodich2017 <31790104+Rodich2017@users.noreply.github.com>
2017-11-06 17:04:28 +0200

    files to process

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import MonthlyPhCosts.arrange
+
+print( "=====================================" )
+print( "====     MONTHLY PHONE COSTS     ====" )
+print( "=====================================", "\n " )
+print( "Enter a path to file: " )
+dat=input()
+
+arrange.take( dat )
+# print(arrange.charges(tot))
+
+
+print( "=====================================" )
+print( "====             END             ====" )
+print( "=====================================" )
+
+exit()
---
+++ arrange.py
@@ -0,0 +1,39 @@
+import os
+import re
+
+def take(dat):
+    if (not os.path.isfile( dat )) or (not os.path.exists( dat )):
+        print( "File not found!" )
+        exit()
+    else:
+        # create new file name
+        newfn, tail = str(os.path.basename(dat)).split('.')
+        outdat = os.path.join(os.path.dirname(dat)) + "\\" +  newfn+"new."+tail
+
+        try:
+            # open file in read mode and save it in another
+            with open( dat, "r", encoding="ANSI" ) as infile:
+              with open( outdat, 'w' ) as outfile:
+                line=infile.readline()
+                cnt=1
+                while line:
+                    print( "Line {}: {}".format( cnt, line.strip() ) )
+                    line=infile.readline()
+                    cnt+=1
+                    line_ckeck = re.match("\"\d",line)
+                    if line_ckeck:
+                        print(str(cnt), " nameren ")
+                        outfile.write(line)
+                outfile.close()
+              infile.close()
+
+        except ValueError:
+            print( ValueError.__name__ )
+        except OSError:
+            print( OSError.errno )
+
+
+        finally:
+            infile.close()
+
+
